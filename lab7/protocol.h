20ab0f16b9c437ddd332072f2a9bffb69dc05f2e
Myse1f <602149025@qq.com>
2018-03-23 11:10:25 +0800

    commit

--- lab7/protocol.h
+++ lab7/protocol.h
@@ -1,13 +1,13 @@
 #ifndef _PROTOCOL_H_
 #define _PROTOCOL_H_
 
-#define MAX_PACKAGE_SIZE 65535
+#define PACKAGE_DATA_SIZE 5000
 
 struct Header {
     Header() { delimiter = 0xffff; }
     unsigned short delimiter;
     unsigned short packageSize;
-    unsigned char end;
+    unsigned char isOver;  //1--the info is over  
     unsigned char type;    
     //0x0? -- require,      0x00--time 0x01--servername 0x02--list 0x03--message
     //0x1? -- response,     0x10--time 0x11--servername 0x12--list 
@@ -16,21 +16,12 @@ struct Header {
 
 struct DataPackage {
     Header header;
-    char *data;
-};
-
-struct TimeFormat {
-    unsigned short year;
-    unsigned short month;
-    unsigned short day;
-    unsigned short hh;
-    unsigned short mm;
-    unsigned short ss; 
+    char data[PACKAGE_DATA_SIZE];
 };
 
 struct ClientInfo {
     unsigned short clientNo;
-    char IPaddress[12];
+    unsigned char IPaddress[4];
     unsigned short port;
 };
 
